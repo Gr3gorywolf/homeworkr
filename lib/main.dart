a1c9883d96e03f76533b41f83d7d8b861cd70602
Gregory <blackarcanjus@gmail.com>
2021-08-17 00:09:47 -0400

    [FIX] some manifest issues

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Homeworkr',
       theme: ThemeData(
         primarySwatch: Colors.teal,
         primaryColor: Color.fromARGB(255, 38, 166, 154),
